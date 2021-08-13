d9abc9381521a26ae56f6009a420cdf4dd994fa8
Lukas <boehm-lukas@outlook.de>
2021-08-13 22:19:58 +0200

    :tada: add vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import Tinybox from "vue-tinybox";
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -9,5 +10,6 @@ Vue.use(Tinybox);
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount("#app");
